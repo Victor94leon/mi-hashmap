6c15763a916eec72b1c068079e018a594a09d418
Víctor <victorlopez94leon@gmail.com>
2016-03-07 12:26:28 +0100

    Añadidos los atributos y el constructor de la clase MiHashMap

---
+++ MiHashMap.java
@@ -0,0 +1,16 @@
+public class MiHashMap
+{
+    // Array para guardar las claves
+    private String[] claves;
+    // Array para guardar los valores
+    private int[] valores;
+
+    /**
+     * Constructor de objetos de la clase MiHashMap
+     */
+    public MiHashMap()
+    {
+        String[] claves = new String[0];
+        int[] valores = new int[0];
+    }
+}
