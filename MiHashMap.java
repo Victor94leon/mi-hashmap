83d804615744707c289e07820e69058ae6d2571f
Víctor <victorlopez94leon@gmail.com>
2016-03-07 12:57:31 +0100

    Añadido el método put para introducir una clave y un valor especificados, sustituyendo el valor si la clave ya existe y devolviendo el valor eliminado o -1 si no hay coincidencias

--- MiHashMap.java
+++ MiHashMap.java
@@ -10,7 +10,40 @@ public class MiHashMap
      */
     public MiHashMap()
     {
-        String[] claves = new String[0];
-        int[] valores = new int[0];
+        claves = new String[0];
+        valores = new int[0];
+    }
+
+    /**
+     * Método que asocia el valor especificado con la calve especificada. Si la clave existía
+     * entonces sobrescribe su valor y devuelve el valor antiguo. Si no existe devuelve -1
+     */
+    public int put(String clave, int valor)
+    {
+        int index = 0;
+        boolean claveRepetida = false;
+        while (index<valores.length && !claveRepetida ) {
+            if (clave == claves[index]) {
+                claveRepetida = true;
+                valores[index] = valor;
+            }
+            index++;
+        }
+        if (!claveRepetida) {
+            index = -1;
+            int cont = 0;
+            String[] nuevasClaves = new String[claves.length + 1];
+            int[] nuevosValores = new int[valores.length + 1];
+            while (cont<claves.length) {
+                nuevasClaves[cont] = claves[cont];
+                nuevosValores[cont] = valores[cont];
+                cont++;
+            }
+            nuevasClaves[nuevasClaves.length - 1] = clave;
+            nuevosValores[nuevosValores.length - 1] = valor;
+            claves = nuevasClaves;
+            valores = nuevosValores;
+        }
+        return index;
     }
 }
